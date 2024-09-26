f5f49b4ac88c745a2b9e99ea8a56076667f5f19a
Daniel Bergmann <bergmann.da@gmx.de>
2024-09-26 23:37:45 +0200

    remove unnec. imports and sprite param from rocket

--- src/rocket.py
+++ src/rocket.py
@@ -1,21 +1,15 @@
-import pygame
 from src.dimensions2d import Dimensions2D
-from src.linear_kinematic import LinearKinematic
 from src.vec2d import Vec2d
-from src.landing_game_object import LandingGameObject
 from src.linear_physical_object import LinearPhysicalObject
 
 
 class Rocket(LinearPhysicalObject):
     def __init__(
         self,
-        sprite: pygame.sprite.Sprite,
         dimensions: Dimensions2D,
         pos: Vec2d,
         mass: float,
         velocity: Vec2d = Vec2d(),
         acceleration: Vec2d = Vec2d(),
     ):
-        LinearPhysicalObject.__init__(
-            sprite, dimensions, pos, mass, velocity, acceleration
-        )
+        LinearPhysicalObject.__init__(dimensions, pos, mass, velocity, acceleration)
