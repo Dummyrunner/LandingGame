0970bba5f7f10a64d698232268fc3aacc2da363e
Daniel Bergmann <bergmann.da@gmx.de>
2024-09-28 10:38:21 +0200

    main: handle all objects in one list, fix landing game object init by calling sprite init

--- src/landing_game_object.py
+++ src/landing_game_object.py
@@ -10,6 +10,13 @@ class LandingGameObject(pygame.sprite.Sprite):
         image: pygame.surface,
         pos: Vec2d = Vec2d(),
     ) -> None:
+        super().__init__()
         self.rect: pygame.Rect = pygame.Surface.get_rect(image)
         self.pos = pos
+        self.rect.center = self.pos
         self.image = image
+
+    def update(self, time_step: float) -> None:
+        # takes timestep argument like physical object,
+        # to be able to treat them in the same way
+        self.rect.center = self.pos
