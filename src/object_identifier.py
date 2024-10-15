7785a6fd6266536eb0e4116920b6db0051d07ec8
beneblubb <benedikt.wohnhas@gmail.com>
2024-10-15 10:50:58 +0200

    Add ObjectIdentifier class for generating unique IDs

---
+++ src/object_identifier.py
@@ -0,0 +1,18 @@
+class ObjectIdentifier:
+    """
+    This class is responsible for generating unique IDs for objects.
+    """
+
+    def __init__(self):
+        self.next_ID = 0
+        self.used_IDs = []
+
+    def get_ID(self):
+        """Returns a unique ID for an object."""
+        self.next_ID += 1
+        self.used_IDs.append(self.next_ID)
+        return self.next_ID
+
+    def get_used_IDs(self):
+        """Returns all IDs that have been used so far."""
+        return self.used_IDs
