50227bc35ab6a2ada51ad1564c78693a04a1a180
beneblubb <benedikt.wohnhas@gmail.com>
2024-10-16 22:44:45 +0200

    Refactor debug overlay and add ID attribute for ground and rocket

--- test/test_id_generator.py
+++ test/test_id_generator.py
@@ -7,14 +7,14 @@ class TestIDGenerator:
     def setup_method(self):
         self.id_generator = IDGenerator()
 
-    def test_generate_id(self):
-        id1 = self.id_generator.generate_id()
-        id2 = self.id_generator.generate_id()
-        assert id1 != id2, "IDs should be unique"
-        assert isinstance(id1, str), "ID should be a string"
-        assert isinstance(id2, str), "ID should be a string"
+    def test_initialize(self):
+        assert self.id_generator.current_id is not None
+        assert self.id_generator.current_id >= 0
+        assert isinstance(self.id_generator.current_id, int)
 
-    def test_id_format(self):
-        id = self.id_generator.generate_id()
-        assert len(id) == 8, "ID should be 8 characters long"
-        assert id.isalnum(), "ID should be alphanumeric"
+    def test_generate_id(self):
+        id1 = self.id_generator.assign_ID()
+        id2 = self.id_generator.assign_ID()
+        assert id1 != id2, "IDs should be unique"
+        assert isinstance(id1, int), "ID should be a integer"
+        assert isinstance(id2, int), "ID should be a integer"
