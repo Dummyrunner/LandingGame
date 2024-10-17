a1e9686ab74b28eab5307af4274465dd91140354
Daniel Bergmann <bergmann.da@gmx.de>
2024-10-17 23:02:05 +0200

    IN-14 renamed linear_kinematics to motion state, establish new getter/setter methods

--- test/test_motion_state.py
+++ test/test_motion_state.py
@@ -1,37 +1,49 @@
 import pytest
-from src.linear_kinematic import LinearKinematic
+from src.motion_state import MotionState
 from src.vec2d import Vec2d
 from src.common_constants import PhysicalBoundaries
 
 ABS_TOLERANCE = 1e-5
+example_pos_meter_vector = Vec2d(1.2, 2.3)
 
 
-class TestLinearKinematic:
+class TestMotionState:
 
     def test_set_mass(self):
-        obj = LinearKinematic(mass=5.0)
+        obj = MotionState(pos_meter=example_pos_meter_vector, mass=5.0)
         assert obj.mass == 5.0
         with pytest.raises(ValueError):
             invalid_mass_value = -1
-            LinearKinematic(mass=invalid_mass_value)
+            MotionState(pos_meter=example_pos_meter_vector, mass=invalid_mass_value)
 
     def test_set_velocity(self):
         velocity_to_assign = Vec2d(111, 222)
-        obj = LinearKinematic(mass=5.0, velocity=velocity_to_assign)
+        obj = MotionState(
+            pos_meter=example_pos_meter_vector, mass=5.0, velocity=velocity_to_assign
+        )
         assert obj.velocity == velocity_to_assign
         velocity_too_large = Vec2d(1e9, 10)
-        obj_too_fast_saturate = LinearKinematic(mass=5.0, velocity=velocity_too_large)
+        obj_too_fast_saturate = MotionState(
+            pos_meter=example_pos_meter_vector, mass=5.0, velocity=velocity_too_large
+        )
         assert obj_too_fast_saturate.velocity.length == pytest.approx(
             PhysicalBoundaries.MAX_SPEED, ABS_TOLERANCE
         )
 
     def test_set_acceleration(self):
         acceleration_to_assign = Vec2d(111, 222)
-        obj = LinearKinematic(mass=5.0, acceleration=acceleration_to_assign)
+        obj = MotionState(
+            pos_meter=example_pos_meter_vector,
+            mass=5.0,
+            acceleration=acceleration_to_assign,
+        )
         assert obj.acceleration == acceleration_to_assign
         acceleration_too_large = Vec2d(1e9, 10)
-        obj_too_accelerated_saturate = LinearKinematic(
-            mass=5.0, velocity=Vec2d(), acceleration=acceleration_too_large
+        obj_too_accelerated_saturate = MotionState(
+            pos_meter=example_pos_meter_vector,
+            mass=5.0,
+            velocity=Vec2d(),
+            acceleration=acceleration_too_large,
         )
         assert obj_too_accelerated_saturate.acceleration.length == pytest.approx(
             PhysicalBoundaries.MAX_SPEED, ABS_TOLERANCE
