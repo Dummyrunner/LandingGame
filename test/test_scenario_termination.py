6d21ac167ade015475ae495cc8b0a6bf9943ad6f
Daniel Bergmann <bergmann.da@gmx.de>
2025-02-18 21:51:22 +0100

    IN-63 add tests for scenario termination struct assignment and stdout print message

--- test/test_scenario_termination.py
+++ test/test_scenario_termination.py
@@ -1,4 +1,5 @@
 from src.scenario_termination import ScenarioTermination
+from src.scenario_results_struct import ScenarioState
 
 some_variable = "hi"
 
@@ -18,3 +19,21 @@ def test_scenario_termination():
 
     assert obj_with_true_condition.termination_condition() == True
     assert obj_with_false_condition.termination_condition() == False
+
+
+def test_scenario_termination_execute_termination(capsys):
+    """Check, whether a message is printed to stdout when termination is executed"""
+    obj_with_true_condition = ScenarioTermination(mock_scenario_true)
+    obj_with_true_condition.execute_termination()
+    captured = capsys.readouterr()
+    assert captured.out.strip() != ""
+
+
+def test_result_struct_assignment():
+    obj = ScenarioTermination(mock_scenario_true)
+    result = obj.result_dict
+    assert result == {}
+    obj.assign_values_to_scenario_result_struct(ScenarioState.SUCCESS, 1.2, 3.4)
+    assert result["scenario_state"] == ScenarioState.SUCCESS
+    assert result["elapsed_time"] == 1.2
+    assert result["final_score"] == 3.4
